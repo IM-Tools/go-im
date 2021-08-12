34d32b5d811d710bc67009405d92b795ee6ccc91
pl1998 <2139714249@qq.com>
2021-08-12 23:31:49 +0800

    文件

--- bin/http/controller/im/baidu_api.go
+++ bin/http/controller/im/baidu_api.go
@@ -6,7 +6,9 @@
 package im
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"go_im/bin/utils"
 	"go_im/pkg/response"
 )
 
@@ -15,12 +17,13 @@ type BaiduController struct {
 }
 func (*BaiduController) UploadVoiceFile(c *gin.Context)  {
 	voice, _ := c.FormFile("voice")
-	//dir := utils.GetCurrentDirectory()
+	dir := utils.GetCurrentDirectory()
 	// 上传文件至指定目录 没找到第三方免费的第三方存储 先用自己的吧
-	path :="/www/wwwroot/qc_admin.pltrue.top/public/voice/"+voice.Filename
+	path :=dir+"/voice/"+voice.Filename
+	fmt.Println(path)
 	 c.SaveUploadedFile(voice, path)
 	response.SuccessResponse(map[string]interface{}{
-		"url":"http://qc_admin.pltrue.top/voice/"+voice.Filename,
+		"url":"https://im.pltrue.top/voice/"+voice.Filename,
 	},200).ToJson(c)
 	return
 }
