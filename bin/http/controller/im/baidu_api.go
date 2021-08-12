4d36e1af6c6dbfb885c6d99bd21e1403f5d29e63
pl1998 <2139714249@qq.com>
2021-08-12 17:58:32 +0800

    调整接口

--- bin/http/controller/im/baidu_api.go
+++ bin/http/controller/im/baidu_api.go
@@ -17,7 +17,7 @@ func (*BaiduController) UploadVoiceFile(c *gin.Context)  {
 	voice, _ := c.FormFile("voice")
 	//dir := utils.GetCurrentDirectory()
 	// 上传文件至指定目录 没找到第三方免费的第三方存储 先用自己的吧
-	path :="/www/wwwroot/voice.pltrue.top/voice/"+voice.Filename
+	path :="/www/wwwroot/qc_admin.pltrue.top/public/voice/"+voice.Filename
 	 c.SaveUploadedFile(voice, path)
 	response.SuccessResponse(map[string]interface{}{
 		"url":"http://voice.pltrue.top/voice/"+voice.Filename,
